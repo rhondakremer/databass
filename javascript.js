3c06af2868ab5b6d050d1f3b6955e6974e8c8b60
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-27 10:05:54 -0400

    can write info from response to page

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
-var queryURL = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_matcher.lyrics.get?q_track=sexy%20and%20i%20know%20it&q_artist=lmfao&page_size=1&page=1&s_track_rating=desc&apikey=4361e89398d1b525228c0f37e4566dc1";
+
+$("#submit").click(function() {
+        var track = $("#track").val().trim();
+        var artist = $("#artist").val().trim();
+        console.log(track);
+        $("#trackInfo").empty();
+
+
+var queryURL = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_matcher.lyrics.get?q_track=" + track + "&q_artist=" + artist + "&page_size=1&page=1&s_track_rating=desc&apikey=4361e89398d1b525228c0f37e4566dc1";
 
 
 $.ajax({
@@ -10,10 +18,12 @@ $.ajax({
 
 }).then(function (response) {
     console.log(response);
+    var trackName = response.message.body.track_list[0].track.track_share_url;
+    $("#trackInfo").prepend(trackName);
     
    
     
 
     });
-
+});
 });
