d9b7ef52572739f4421d05797dfd0c7f137abfee
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-27 11:25:06 -0400

    sometimes it works

--- javascript.js
+++ javascript.js
@@ -19,11 +19,21 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
     var trackName = response.message.body.track_list[0].track.track_share_url;
-    $("#trackInfo").prepend(trackName);
-    
-   
-    
+    //$("#trackInfo").prepend(trackName);
+    var trackID = response.message.body.track_list[0].track.track_id;
+    console.log(trackID);
 
+    var queryURL2 = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackID + "&apikey=4361e89398d1b525228c0f37e4566dc1"
+
+        $.ajax ({
+        url: queryURL2,
+        dataType: "json",
+        method: "GET" 
+        }).then(function (response) {
+        console.log(response);
+        var lyrics = response.message.body.lyrics.lyrics_body;
+        $("#lyrics").prepend(lyrics);
     });
+});
 });
 });
