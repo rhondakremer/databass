c8749ef2de7ab0fa66d0299152d77befb8036f6d
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-27 11:28:47 -0400

    now it works for all test songs

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ $("#submit").click(function() {
         $("#trackInfo").empty();
 
 
-var queryURL = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_matcher.lyrics.get?q_track=" + track + "&q_artist=" + artist + "&page_size=1&page=1&s_track_rating=desc&apikey=4361e89398d1b525228c0f37e4566dc1";
+var queryURL = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=" + track + "&q_artist=" + artist + "&page_size=1&page=1&s_track_rating=desc&apikey=4361e89398d1b525228c0f37e4566dc1";
 
 
 $.ajax({
