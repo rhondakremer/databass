940571a90ddf3ea436e7b53a0ccfa66fff814f6d
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-27 13:36:56 -0400

    Does not accept misspelled track names

--- javascript.js
+++ javascript.js
@@ -18,10 +18,14 @@ $.ajax({
 
 }).then(function (response) {
     console.log(response);
-    var trackName = response.message.body.track_list[0].track.track_share_url;
     //$("#trackInfo").prepend(trackName);
+    if (response.message.body.track_list.length == 0) {
+        alert("please check your spelling!")
+    } else {
     var trackID = response.message.body.track_list[0].track.track_id;
     console.log(trackID);
+
+    
 
     var queryURL2 = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackID + "&apikey=4361e89398d1b525228c0f37e4566dc1"
 
@@ -29,11 +33,13 @@ $.ajax({
         url: queryURL2,
         dataType: "json",
         method: "GET" 
+
         }).then(function (response) {
         console.log(response);
         var lyrics = response.message.body.lyrics.lyrics_body;
         $("#lyrics").prepend(lyrics);
     });
+}
 });
 });
 });
